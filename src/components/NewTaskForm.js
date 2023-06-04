a65a43e8bbcfd89813d6a6b7b1ad097b8cb58f41
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-06-04 14:08:11 -0700

    complete lab

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSubmit,categories}) {
+
+  const [newItemFields,setNewItemFields]= useState({
+    text:'',
+    category:'Code'
+  })
+
+  function handleFields(event){
+    const {name,value}=event.target
+    setNewItemFields({...newItemFields,[name]:value})
+  }
   return (
-    <form className="new-task-form">
+    <form onSubmit={(event)=>{
+      event.preventDefault()
+      onTaskFormSubmit(newItemFields)
+      }} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input onChange={handleFields} value={newItemFields.text} type="text" name="text" />
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category" value={newItemFields.category} onChange={handleFields}>
           {/* render <option> elements for each category here */}
+          {categories.map((category,index)=>(<option key={index}>{category}</option>))}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks,deletedItem}) {
+  //const [tasksList, setTaskList] = useState(TASKS);
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      <ul className="Items">
+        {tasks.map((item, index) => (<Task deletedItem={deletedItem} key={index} text={item.text} category={item.category }/>))}
+      </ul>
     </div>
   );
 }
